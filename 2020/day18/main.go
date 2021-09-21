afd7f711298a64aa99622126e1ca3c4ac2fc03c9
Davide Masserut <dm@mssdvd.com>
2021-09-21 16:47:47 +0200

    Add day 18 solution (Part 1)

---
+++ 2020/day18/main.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	f, err := os.Open("./input")
+	check(err)
+	defer f.Close()
+
+	var sum int
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		var stack []int
+		var opStack []rune
+		// Shunting-yard algorithm
+		for _, el := range s.Text() {
+			val, err := strconv.Atoi(string(el))
+			if err == nil {
+				stack = append(stack, val)
+				continue
+			}
+			switch el {
+			case '*', '+':
+				for checkOpenParen(opStack) {
+					calc(&stack, &opStack)
+				}
+				opStack = append(opStack, el)
+			case '(':
+				opStack = append(opStack, el)
+			case ')':
+				for checkOpenParen(opStack) {
+					calc(&stack, &opStack)
+				}
+				opStack = opStack[:len(opStack)-1]
+			case ' ':
+				continue
+			}
+		}
+		for checkOpenParen(opStack) {
+			calc(&stack, &opStack)
+		}
+		sum += stack[0]
+	}
+	check(s.Err())
+
+	fmt.Printf("sum: %v\n", sum)
+}
+
+func checkOpenParen(opStack []rune) bool {
+	return len(opStack) > 0 && opStack[len(opStack)-1] != '('
+}
+
+func calc(stack *[]int, opStack *[]rune) {
+	n := len(*stack)
+	var op rune
+	op, *opStack = (*opStack)[len(*opStack)-1], (*opStack)[:len(*opStack)-1]
+	switch op {
+	case '*':
+		(*stack)[n-2] = (*stack)[n-2] * (*stack)[n-1]
+	case '+':
+		(*stack)[n-2] = (*stack)[n-2] + (*stack)[n-1]
+	}
+	*stack = (*stack)[:n-1]
+}
