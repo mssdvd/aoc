ecbe5ecdc288d4bba508e515e302e2f2dbf24361
Davide Masserut <dm@mssdvd.com>
2021-11-21 16:23:45 +0100

    template.go: swap main() and check()

--- 2020/template.go
+++ 2020/template.go
@@ -5,12 +5,6 @@ import (
 	"os"
 )
 
-func check(err error) {
-	if err != nil {
-		panic(err)
-	}
-}
-
 func main() {
 	f, err := os.Open("./input")
 	check(err)
@@ -18,7 +12,12 @@ func main() {
 
 	s := bufio.NewScanner(f)
 	for s.Scan() {
-
 	}
 	check(s.Err())
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
